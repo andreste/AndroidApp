c3db9dc06c8fa50560f978b548bb408e15abab10
Andres Trevino <andreste24@gmail.com>
2024-10-22 12:35:38 -0400

    Show notifications

--- app/src/main/java/com/andreste/whop/screens/HomeScreen.kt
+++ app/src/main/java/com/andreste/whop/screens/HomeScreen.kt
@@ -1,9 +1,33 @@
 package com.andreste.whop.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.andreste.whop.models.WhopNotification
 import com.andreste.whop.viewmodels.NotificationsViewModel
+import com.andreste.whop.viewmodels.ViewState
 
 @Composable
 fun HomeScreen(navController: NavController, viewModel: NotificationsViewModel) {
@@ -11,4 +35,108 @@ fun HomeScreen(navController: NavController, viewModel: NotificationsViewModel) 
         viewModel.start(navController)
         viewModel.getNotifications()
     }
+
+    viewModel.state.collectAsState().value.let { state ->
+        when (state) {
+            is ViewState.Content -> {
+                if (state.list.isEmpty()) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(
+                            text = "No stocks found",
+                            modifier = Modifier.padding(16.dp),
+                            textAlign = TextAlign.Center
+                        )
+                    }
+
+                } else {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .fillMaxWidth()
+                    ) {
+                        items(state.list) {
+                            NotificationRow(it)
+                        }
+                    }
+                }
+            }
+
+            is ViewState.Error -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Error ${state.message}",
+                        modifier = Modifier.padding(16.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+            ViewState.Loading -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(48.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun NotificationRow(notification: WhopNotification) {
+    Row(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (!notification.read) {
+            Box(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(16.dp)
+                    .background(Color.Blue, shape = CircleShape)
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+        ) {
+            Text(text = notification.title)
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = notification.timestamp
+            )
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+            .background(Color.Gray)
+    )
+}
+
+@Preview
+@Composable
+fun NotificationRowPreview() {
+    NotificationRow(notification = WhopNotification(id = "1", title = "Title", message = "Message", read = false, timestamp = "timestamp"))
 }
